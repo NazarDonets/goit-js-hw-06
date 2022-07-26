c7e041fe6682814dd2ef4581f032ae9b395f7f98
Nazar D <nazardonets@gmail.com>
2022-07-26 21:56:42 +0300

    finished first task

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((elem) => {
+	console.log(`Category: ${elem.firstElementChild.textContent}`);
+	console.log(`Elements: ${elem.querySelectorAll('li').length}`);
+});
